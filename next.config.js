753749a457df978c4a087c63d11ff8ba9acda165
bd9356-gif <bd9356@gmail.com>
2026-04-10 08:59:15 -0400

    fix: remove timeout 0 from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-  staticPageGenerationTimeout: 0,
 }
 module.exports = nextConfig
