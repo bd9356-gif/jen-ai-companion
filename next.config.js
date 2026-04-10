408a043945e877243058204960daa4b69b13883a
bd9356-gif <bd9356@gmail.com>
2026-04-10 08:33:26 -0400

    fix: disable prerender errors

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
+  experimental: { missingSuspenseWithCSRBailout: false },
 }
 module.exports = nextConfig
