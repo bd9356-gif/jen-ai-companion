600bc942e7d2753c19d753bfad21a89f336e908f
bd9356-gif <bd9356@gmail.com>
2026-04-12 20:47:35 -0400

    fix: explore JSX error

--- app/explore/page.js
+++ app/explore/page.js
@@ -365,7 +365,7 @@ export default function ExplorePage() {
                   </div>
                 </div>
               )}
-            )}
+            </div>
           </div>
         ) : (
           <div>
