7751b7628d76643bdb06a7ab9f5970111bf49565
bd9356-gif <bd9356@gmail.com>
2026-05-10 10:51:53 -0400

    feat: guides - add Chef Jen link in header next to Chef TV

--- app/guides/page.js
+++ app/guides/page.js
@@ -181,6 +181,7 @@ export default function GuidesPage() {
             )}
           </div>
           <div className="flex items-center gap-2">
+            <button onClick={() => window.location.href='/chef'} className="text-xs text-gray-500 border border-gray-200 rounded-lg px-3 py-1.5">Chef Jen</button>
             <button onClick={() => window.location.href='/videos'} className="text-xs text-gray-500 border border-gray-200 rounded-lg px-3 py-1.5">Chef TV</button>
           </div>
         </div>
