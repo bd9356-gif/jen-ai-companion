20f054906465d2c35b7cdbd744a12b69167d249e
bd9356-gif <bd9356@gmail.com>
2026-05-26 12:39:09 -0400

    fix: quiz result links to Learning Vault not Playbook

--- app/study/[articleId]/page.js
+++ app/study/[articleId]/page.js
@@ -209,10 +209,10 @@ export default function StudyHallPage({ params }) {
               Back to the article
             </button>
             <button
-              onClick={() => window.location.href = '/playbook'}
+              onClick={() => window.location.href = '/secret?view=cardbox'}
               className="px-4 py-2 bg-white border-2 border-gray-200 text-gray-600 rounded-xl text-sm font-semibold hover:border-gray-300"
             >
-              See my Playbook
+              See Learning Vault
             </button>
           </div>
         </main>
