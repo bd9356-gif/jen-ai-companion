363f1d71146f580c5d11bfd338fc3194b3ae2af3
Bill <bill@mycompanionapps.com>
2026-06-21 12:55:05 +0000

    Fix iPad sidebar navigation - reset NavigationPath on sidebar selection change

--- ios/App/App/MyRecipe Companion/Views/MainTabView.swift
+++ ios/App/App/MyRecipe Companion/Views/MainTabView.swift
@@ -5,6 +5,7 @@ struct MainTabView: View {
     @Binding var selectedTab: Int
     @Environment(\.horizontalSizeClass) var sizeClass
     @State private var sidebarSelection: Int? = 0
+    @State private var navPath = NavigationPath()
 
     var body: some View {
         if sizeClass == .regular {
@@ -19,7 +20,7 @@ struct MainTabView: View {
                 .navigationTitle("MyRecipe")
                 .accentColor(.orange)
             } detail: {
-                NavigationStack {
+                NavigationStack(path: $navPath) {
                     switch sidebarSelection ?? 0 {
                     case 0: KitchenHubView().environmentObject(authManager)
                     case 1: RecipeVaultView().environmentObject(authManager)
@@ -31,6 +32,9 @@ struct MainTabView: View {
                 }
             }
             .accentColor(.orange)
+            .onChange(of: sidebarSelection) { _, _ in
+                navPath = NavigationPath()
+            }
         } else {
             TabView(selection: $selectedTab) {
                 NavigationView {
